b4db8127f04bc9680e816ce5221c0ab3f98afa2f
Shmuel Meymann <shmuel.meymann@verint.com>
2019-12-26 12:18:34 +0200

    Forgotten files

---
+++ ChatClient/app/src/main/java/com/playground/chatclient/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.playground.chatclient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().add(R.id.mainScreenArena, ChatWindowFragment(), "Chat").commit()
+    }
+}
